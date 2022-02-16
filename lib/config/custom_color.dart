3610e7bec6126b99fa20443137d092932134fa76
unknownunnamed <fikriar19@student.untan.ac.id>
2022-02-16 10:29:22 +0700

    add custom color

---
+++ lib/config/custom_color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static final Color primaryRose = Color(0xffFF005D);
+  static final Color secondaryGreen = Color(0xff00A650);
+  static final Color secondaryTorea = Color(0xff1919A1);
+  static final Color secondaryGold = Color(0xffFFD600);
+  static final Color neutralBlack = Color(0xff292929);
+  static final Color neutralWhite = Color(0xffFAFAFA);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Siskom Door',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
           primarySwatch: Colors.blue,
           fontFamily: "Poppins",
